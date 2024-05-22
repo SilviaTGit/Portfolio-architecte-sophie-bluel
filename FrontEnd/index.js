c5e053549e0b8dfa8a91daa02e3fe02a0e544051
SilviaTGit <163752329+SilviaTGit@users.noreply.github.com>
2024-05-22 15:01:48 +0200

    Getting works back from the backend

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,20 +1,27 @@
-const galerie = document.querySelector(".gallery");
-// Requête Fetch pour obtenir les données JSON
-fetch("http://localhost:5678/api/works")
-  .then((response) => response.json()) // Convertir la réponse en JSON
-  .then((data) => {
-    for (const work of data) {
-      // Création d'un élément figure pour l'œuvre
-      const figureElement = document.createElement("figure");
-      // Création d'un élément de paragraphe pour le titre de l'œuvre
-      const titreElement = document.createElement("p");
-      titreElement.textContent = work.title;
-      // Création d'un élément d'image pour l'URL de l'image de l'œuvre
-      const imageElement = document.createElement("img");
-      imageElement.src = work.imageUrl;
-      // Ajout de figure, titre et de l'image à la galerie
-      galerie.appendChild(figureElement);
-      galerie.appendChild(titreElement);
-      galerie.appendChild(imageElement);
-    }
+/* Variables */
+const gallery = document.querySelector(".gallery");
+
+/* Function that returns the list of works */
+async function getWorks() {
+  const response = await fetch ("http://localhost:5678/api/works");
+  return await response.json();
+}
+
+getWorks();
+
+/* Show works in the DOM */
+async function displayWorks() {
+  const arrayWorks = await getWorks()
+  arrayWorks.forEach(work => {
+    const figure = document.createElement("figure")
+    const img = document.createElement("img")
+    const figcaption = document.createElement("figcaption")
+    img.src = work.imageUrl;
+    figcaption.textContent = work.title;
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    gallery.appendChild(figure);
   });
+}
+
+displayWorks();
